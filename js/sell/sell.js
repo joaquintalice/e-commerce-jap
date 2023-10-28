098f581f142359a177d56fc26b88163abb835585
Joaquin Talice <joaquintalice@gmail.com>
2023-10-28 13:32:26 -0300

    fix: Spinner&&getJSONData function

--- js/sell/sell.js
+++ js/sell/sell.js
@@ -10,7 +10,7 @@ let PERCENTAGE_SYMBOL = '%';
 let MSG = "FUNCIONALIDAD NO IMPLEMENTADA";
 
 //Función que se utiliza para actualizar los costos de publicación
-function updateTotalCosts(){
+function updateTotalCosts() {
     let unitProductCostHTML = document.getElementById("productCostText");
     let comissionCostHTML = document.getElementById("comissionText");
     let totalCostHTML = document.getElementById("totalCostText");
@@ -27,39 +27,37 @@ function updateTotalCosts(){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("productCountInput").addEventListener("change", function(){
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("productCountInput").addEventListener("change", function () {
         productCount = this.value;
         updateTotalCosts();
     });
 
-    document.getElementById("productCostInput").addEventListener("change", function(){
+    document.getElementById("productCostInput").addEventListener("change", function () {
         productCost = this.value;
         updateTotalCosts();
     });
 
-    document.getElementById("goldradio").addEventListener("change", function(){
+    document.getElementById("goldradio").addEventListener("change", function () {
         comissionPercentage = 0.13;
         updateTotalCosts();
     });
-    
-    document.getElementById("premiumradio").addEventListener("change", function(){
+
+    document.getElementById("premiumradio").addEventListener("change", function () {
         comissionPercentage = 0.07;
         updateTotalCosts();
     });
 
-    document.getElementById("standardradio").addEventListener("change", function(){
+    document.getElementById("standardradio").addEventListener("change", function () {
         comissionPercentage = 0.03;
         updateTotalCosts();
     });
 
-    document.getElementById("productCurrency").addEventListener("change", function(){
-        if (this.value == DOLLAR_CURRENCY)
-        {
+    document.getElementById("productCurrency").addEventListener("change", function () {
+        if (this.value == DOLLAR_CURRENCY) {
             MONEY_SYMBOL = DOLLAR_SYMBOL;
-        } 
-        else if (this.value == PESO_CURRENCY)
-        {
+        }
+        else if (this.value == PESO_CURRENCY) {
             MONEY_SYMBOL = PESO_SYMBOL;
         }
 
@@ -69,10 +67,10 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     //Configuraciones para el elemento que sube archivos
     let dzoptions = {
-        url:"/",
+        url: "/",
         autoQueue: false
     };
-    let myDropzone = new Dropzone("div#file-upload", dzoptions);    
+    let myDropzone = new Dropzone("div#file-upload", dzoptions);
 
 
     //Se obtiene el formulario de publicación de producto
@@ -80,9 +78,9 @@ document.addEventListener("DOMContentLoaded", function(e){
 
     //Se agrega una escucha en el evento 'submit' que será
     //lanzado por el formulario cuando se seleccione 'Vender'.
-    sellForm.addEventListener("submit", function(e){
+    sellForm.addEventListener("submit", function (e) {
 
-        e.preventDefault(); 
+        e.preventDefault();
         e.preventDefault();
 
         let productNameInput = document.getElementById("productName");
@@ -98,49 +96,77 @@ document.addEventListener("DOMContentLoaded", function(e){
         //Se realizan los controles necesarios,
         //En este caso se controla que se haya ingresado el nombre y categoría.
         //Consulto por el nombre del producto
-        if (productNameInput.value === "")
-        {
+        if (productNameInput.value === "") {
             productNameInput.classList.add('is-invalid');
             infoMissing = true;
         }
-        
+
         //Consulto por la categoría del producto
-        if (productCategory.value === "")
-        {
+        if (productCategory.value === "") {
             productCategory.classList.add('is-invalid');
             infoMissing = true;
         }
 
         //Consulto por el costo
-        if (productCost.value <=0)
-        {
+        if (productCost.value <= 0) {
             productCost.classList.add('is-invalid');
             infoMissing = true;
         }
-        
-        if(!infoMissing)
-        {
+
+        if (!infoMissing) {
             //Aquí ingresa si pasó los controles, irá a enviar
             //la solicitud para crear la publicación.
+            const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
 
-            getJSONData(PUBLISH_PRODUCT_URL).then(function(resultObj){
+            let showSpinner = function () {
+                document.getElementById("spinner-wrapper").style.display = "block";
+            }
+
+            let hideSpinner = function () {
+                document.getElementById("spinner-wrapper").style.display = "none";
+            }
+
+            const getJSONData = (url) => {
+                let result = {};
+                showSpinner();
+                return fetch(url)
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw Error(response.statusText);
+                        }
+                    })
+                    .then((response) => {
+                        result.status = 'ok';
+                        result.data = response;
+                        hideSpinner();
+                        return result;
+                    })
+                    .catch((error) => {
+                        result.status = 'error';
+                        result.data = error;
+                        hideSpinner();
+                        return result;
+                    });
+            }
+
+            getJSONData(PUBLISH_PRODUCT_URL).then(function (resultObj) {
                 let msgToShowHTML = document.getElementById("resultSpan");
                 let msgToShow = "";
-    
+
                 //Si la publicación fue exitosa, devolverá mensaje de éxito,
                 //de lo contrario, devolverá mensaje de error.
                 //FUNCIONALIDAD NO IMPLEMENTADA
-                if (resultObj.status === 'ok')
-                {
+                if (resultObj.status === 'ok') {
                     msgToShow = MSG;
                     document.getElementById("alertResult").classList.add('alert-primary');
                 }
-                else if (resultObj.status === 'error')
-                {
+                else if (resultObj.status === 'error') {
                     msgToShow = MSG;
                     document.getElementById("alertResult").classList.add('alert-primary');
                 }
-    
+
                 msgToShowHTML.innerHTML = msgToShow;
                 document.getElementById("alertResult").classList.add("show");
             });
