35f535041cad6c22cca3162f189da293d7eddf30
juabiDev <Mederos40@outlook.com>
2023-11-16 08:38:21 -0300

    refactor: darkmode

--- js/darkmode/darkmode.js
+++ js/darkmode/darkmode.js
@@ -21,7 +21,7 @@ function main() {
     } else {
 
         if (JSON.parse(previewDefaultColor) === "true") {
-            initialDarkMode()
+            applyDarkStyles()
         }
     }
 
@@ -37,84 +37,10 @@ darkModeBtn.addEventListener('click', () => {
     loadDarkModeState()
 });
 
-function initialDarkMode() {
-
-    if (body.classList.contains('dark-mode')) {
-        const cards = document.getElementsByClassName("card")
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].classList.add("dark-default")
-        }
-
-        const btns = document.getElementsByClassName("btn")
-
-        for (let i = 0; i < btns.length; i++) {
-            btns[i].classList.add("dark-default")
-        }
-
-        const tables = document.getElementsByClassName("table")
-
-        for (let i = 0; i < tables.length; i++) {
-            tables[i].classList.add("table-dark")
-        }
-
-        const inputs = document.getElementsByTagName("input")
-
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].classList.add("dark-default")
-        }
-
-        const inputsGroup = document.getElementsByClassName("input-group-text")
-
-        for (let i = 0; i < inputsGroup.length; i++) {
-            inputsGroup[i].classList.add("dark-default")
-        }
-
-        const selects = document.getElementsByTagName("select")
-
-        for (let i = 0; i < selects.length; i++) {
-            selects[i].classList.add("dark-default")
-        }
-
-        const listItems = document.getElementsByClassName("resume-list")
-
-        for (let i = 0; i < listItems.length; i++) {
-            listItems[i].classList.add("dark-default")
-            listItems[i].style = "pointer-events:none;"
-        }
-
-        const textAreas = document.getElementsByTagName("textarea")
-
-        for (let i = 0; i < textAreas.length; i++) {
-            textAreas[i].classList.add("dark-default")
-        }
-
-        const modals = document.getElementsByClassName("modal-content")
-
-        for (let i = 0; i < modals.length; i++) {
-            modals[i].classList.add("dark-default")
-        }
-
-        const fileUpload = document.getElementsByClassName("file-upload-div")
-
-        for (let i = 0; i < fileUpload.length; i++) {
-            fileUpload[i].classList.add("dark-default")
-        }
-
-    }
-}
-
 function switchMode() {
     body.classList.toggle('dark-mode');
     storeDarkModeState(body.classList.contains('dark-mode'));
-    cardsStyle();
-    buttonStyle();
-    tableStyle();
-    inputStyle();
-    selectStyle();
-    listStyle();
-    textAreaStyle();
-    modalStyle();
-    fileUploadDescriptionStyle();
+    applyDarkStyles()
 }
 
 function storeDarkModeState(value) {
@@ -131,76 +57,55 @@ function loadDarkModeState() {
     }
 }
 
-function tableStyle() {
-    const cards = document.getElementsByClassName("table")
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].classList.toggle("table-dark")
+function applyDarkStyles() {
+    const elementsToStyle = [
+        ...document.getElementsByClassName("card"),
+        ...document.getElementsByClassName("btn"),
+        ...document.getElementsByTagName("input"),
+        ...document.getElementsByClassName("input-group-text"),
+        ...document.getElementsByTagName("select"),
+        ...document.getElementsByClassName("resume-list"),
+        ...document.getElementsByTagName("textarea"),
+        ...document.getElementsByClassName("modal-content"),
+        ...document.getElementsByClassName("file-upload-div")
+
+    ];
+
+    const tables = [...document.getElementsByClassName("table")]
+
+    if(tables) {
+        tables.forEach((table) => {
+            table.classList.add('table-dark');
+        });
     }
-}
-
-function cardsStyle() {
-    const cards = document.getElementsByClassName("card")
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].classList.toggle("dark-default")
-    }
-}
-
-function inputStyle() {
-    const inputs = document.getElementsByTagName("input")
-
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].classList.toggle("dark-default")
-    }
-
-    const inputsGroup = document.getElementsByClassName("input-group-text")
-
-    for (let i = 0; i < inputsGroup.length; i++) {
-        inputsGroup[i].classList.toggle("dark-default")
-    }
-}
-
-function selectStyle() {
-    const selects = document.getElementsByTagName("select")
-    for (let i = 0; i < selects.length; i++) {
-        selects[i].classList.toggle("dark-default")
-    }
-}
-
-function listStyle() {
-    const listItems = document.getElementsByClassName("resume-list")
-
-    for (let i = 0; i < listItems.length; i++) {
-        listItems[i].classList.toggle("dark-default")
-    }
-}
-
-function buttonStyle() {
-    const btns = document.getElementsByClassName("btn")
-
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].classList.toggle("dark-default")
-    }
-}
-
-function textAreaStyle() {
-    const textAreas = document.getElementsByTagName("textarea")
-
-    for (let i = 0; i < textAreas.length; i++) {
-        textAreas[i].classList.toggle("dark-default")
-    }
-}
-
-function modalStyle() {
-    const modals = document.getElementsByClassName("modal-content")
-
-    for (let i = 0; i < modals.length; i++) {
-        modals[i].classList.toggle("dark-default")
-    }
-}
-
-function fileUploadDescriptionStyle() {
-    const fileUpload = document.getElementsByClassName("file-upload-div")
-    fileUpload[0] ? fileUpload[0].classList.toggle("dark-default") : null
+    
+    elementsToStyle.forEach((element) => {
+        if (body.classList.contains('dark-mode')) {
+            element.classList.add('dark-default');
+        } else {
+            element.classList.remove('dark-default');
+        }
+    });
 
 }
 
+function applyDarkStyles2() {
+    const elementsToStyle = [
+        ...document.getElementsByClassName("card"),
+        ...document.getElementsByClassName("btn"),
+        ...document.getElementsByClassName("table"),
+        ...document.getElementsByTagName("input"),
+        ...document.getElementsByClassName("input-group-text"),
+        ...document.getElementsByTagName("select"),
+        ...document.getElementsByClassName("resume-list"),
+        ...document.getElementsByTagName("textarea"),
+        ...document.getElementsByClassName("modal-content"),
+        ...document.getElementsByClassName("file-upload-div")
+
+    ];
+        
+    elementsToStyle.forEach((element) => {
+        element.classList.add('dark-default');
+    });
+
+}
